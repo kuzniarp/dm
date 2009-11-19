0a2eb2a9b6de8540ecdd9c01786c6c3e820da22f
kuzniarp <kuzniarp@gmail.com>
2009-11-19 16:35:55 +0100

    Changed frontend and admin layouts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,10 @@ module ApplicationHelper
     html = ''
     html += '<ul id="subpage_list_' + pages.first.parent_id.to_s + '"'
     pages.each do |page|
-      html += '<li class="subpage" id="subpage_' + page.id.to_s + '">'
-      html += image_tag "/images/sort_btn.png", :style => "cursor:pointer;"
-      html += link_to page.name, :controller => 'subpage', :action => 'edit', :id => page.id
-      html += link_to(image_tag("/images/delete_btn.png"), {:controller => 'subpage', :action => 'delete', :id => page.id}, :confirm => "Jesteś pewien, że chcesz usunąć tę stronę?")
-      html += '</li>'
+      html += '<li class="subpage" id="subpage_' + page.id.to_s + '"><div>'
+      html += link_to page.name, {:controller => 'subpage', :action => 'edit', :id => page.id}, :class => "label"
+      html += '<span>' + link_to(image_tag("/images/admin/remove.png"), {:controller => 'subpage', :action => 'delete', :id => page.id}, :confirm => "Jesteś pewien, że chcesz usunąć tę stronę?")
+      html += '</span></div></li>'
     end
     html += '</ul>'
     html
