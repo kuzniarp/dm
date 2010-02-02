1c52d3d6353dcf4617a5e14fca253bcfaecc48c9
kuzniarp <kuzniarp@gmail.com>
2010-02-02 23:57:49 +0100

    Extended producers/products.

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,6 +1,6 @@
 class Producer < ActiveRecord::Base
   has_many :products
   belongs_to :subpage
-
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :path => ":rails_root/public/system/:attachment/:class/:id/:style/:filename", :url => "/system/:attachment/:class/:id/:style/:filename"
   has_permalink :name, :unique => true, :to_param => :permalink
 end
