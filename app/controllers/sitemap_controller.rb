b495d6d5781c8fe1615313985a586d8238380ed0
kuzniarp <kuzniarp@gmail.com>
2010-02-14 17:14:10 +0100

    Sitemap modified.

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,6 +1,8 @@
 class SitemapController < ApplicationController
 	def sitemap
 		@subpages = Subpage.find(:all, :order => 'page_order')
+    @producers = Producer.all
+    @products = Product.all
 		headers["Content-Type"] = "text/xml"
 		# set last modified header to the date of the latest entry.
 		#headers["Last-Modified"] = @entries[0].updated_at.httpdate    
